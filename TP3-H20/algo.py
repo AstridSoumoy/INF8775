ca26902c7e27c570df4a29e42166617d9c5987ea
Axel Templier <scampieamt@gmail.com>
2020-04-05 17:59:19 -0400

    ajout de fichier

--- TP3-H20/algo.py
+++ TP3-H20/algo.py
@@ -2,10 +2,11 @@ import sys
 import csv
 import numpy as np
 import time
-import pandas as pd 
+import pandas as pd
+import sys
 
 
-def readFile(arg): 
+def readFile(arg):
     file = open(arg, "r")
     A = []
     count = 0
@@ -22,8 +23,39 @@ def readFile(arg):
     A.remove(A[count - 2])
     return premiereLigne, derniereLigne, A
 
+def findSolution(graph, listInfectee, k):
+    print(getPourcentagePersonneInfectee(graph, listInfectee, k))
+
+def getPourcentagePersonneInfectee(graph, listInfectee, k):
+    verificationEntourage(graph, listInfectee, k)
+    return (100* len(listInfectee))/len(graph)
+
+
+
+def estInfectee(ligne, listInfectee, k, graph):
+    count = 0
+    for i in range(0, len(graph)):
+        if (i in listInfectee) and (ligne[i] == 1):
+            count += 1
+    if count >= k:
+        return True
+    else:
+        return False
+
+
+def verificationEntourage(graph, listInfectee, k):
+    for i in range(0, len(graph)):
+        print(listInfectee)
+        if (not (i in listInfectee)) & estInfectee(graph[i], listInfectee, k, graph):
+            listInfectee.append(i)
+            verificationEntourage(graph, listInfectee, k)
+
+
+
+
 
 def main(argv):
+    #sys.setrecursionlimit(10**6)
     file = open(argv[0], "r")
     lines = file.readlines()
     countLines = len(lines)
@@ -42,12 +74,18 @@ def main(argv):
 
     notes = readFile(argv[0])
     PermiereLigne = list(notes[0])
-    DerniereLigne = list(notes[1])
+    tailleDeLaPopulation = PermiereLigne[0]
+    nombrePersonnesMalades = PermiereLigne[1]
+    PersonnesInfectes = list(notes[1])
     A = list(notes[2])
     graph = np.array([np.array(list(mapGraph)) for mapGraph in A])
     print(PermiereLigne)
-    print(DerniereLigne)
+    print(PersonnesInfectes)
     print(graph)
+    k = argv[1]
+    if argv[2] == "-p":
+        print("-p entered")
+    findSolution(graph, PersonnesInfectes, k)
     return
 
 if __name__ == "__main__":
---
+++ TP3-H20/tp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+OPTIONS=""
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -e)
+    EX_PATH_1="$2"
+    shift
+    ;;
+    -k)
+    TAUX="$2"
+    shift
+    ;;
+    -p)
+    OPTIONS="${OPTIONS}${1} "
+    ;;
+    *)
+        echo "Argument inconnu: ${1}"
+        exit
+    ;;
+esac
+shift
+done
+python ./algo.py $EX_PATH_1 $TAUX $OPTIONS
